e00c1af2bd21289248a10a906118346cef506987
qun <1366895462@qq.com>
2020-06-21 15:13:44 +0800

    Signed-off-by: qun <1366895462@qq.com>

---
+++ spring-security-oauth2-server/src/main/java/com/xiaoyin/oauth2/server/config/AuthorizationServerConfiguration.java
@@ -0,0 +1,26 @@
+package com.xiaoyin.oauth2.server.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+
+        clients
+                .inMemory()
+                .withClient("client")
+                .secret(passwordEncoder.encode("secret"))
+                .authorizedGrantTypes("authorization_code")
+                .scopes("app")
+                .redirectUris("http://www.baidu.com");
+    }
+}
